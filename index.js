5a70c20b1f6e325dd7a5c8e81d9dbddd34337569
Matheus Vicente <matt.vicent@gmail.com>
2018-12-22 12:55:04 -0200

    Fixed production and dev scripts

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, function() {
-  console.log('JSON Server is running on port' + port);
+  console.log('Server is running on port ' + port);
 });
